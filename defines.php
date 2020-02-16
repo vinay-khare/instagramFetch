4ea69c554dea914a37371cb3097a7d2bd9a8d338
vinay-khare <46798981+vinay-khare@users.noreply.github.com>
2020-02-16 16:38:46 +0530

    Add files via upload

---
+++ defines.php
@@ -0,0 +1,4 @@
+<?php
+	define( 'INSTAGRAM_APP_ID', '196009974843635' );
+	define( 'INSTAGRAM_APP_SECRET', '3d84c104ec50b475e62c0ddc9b168a4a' );
+	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://vinay-khare.github.io/instagramFetch/instagram_basic_display_api.php' );
